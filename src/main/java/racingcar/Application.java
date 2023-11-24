e018be3fe88bac7aa897ff9d4281e3abb43e4fc8
yooyoungpark <yoongtopiaa@gmail.com>
2023-11-24 15:43:40 +0900

    feat : 0에서 9 사이에서 무작위 값 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,16 +1,17 @@
 package racingcar;
 
 import java.util.List;
+import racingcar.domain.NumberGenerator;
 import racingcar.view.InputView;
 
 public class Application {
 
     public static void main(String[] args) {
         List<String> carNames = InputView.inputCarName();
-        System.out.println(carNames);
         int gameCount = InputView.inputGameCount();
-        System.out.println(gameCount);
-
+        NumberGenerator numberGenerator = new NumberGenerator();
+        List<Integer> randomNumbers = numberGenerator.createRandomNumbers(carNames);
+        System.out.println(randomNumbers);
 
     }
 }
--- src/main/java/racingcar/domain/NumberGenerator.java
+++ src/main/java/racingcar/domain/NumberGenerator.java
@@ -1,11 +1,16 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class NumberGenerator {
-    public int createRandomNumbers() {
-        int randomNum = Randoms.pickNumberInRange(0,9);
-        return randomNum;
+    public List<Integer> createRandomNumbers(List<String> carName) {
+        List<Integer> randomNumbers = new ArrayList<>();
+        for (int i = 0; i < carName.size(); i++) {
+            int number = Randoms.pickNumberInRange(0,9);
+            randomNumbers.add(number);
+        }
+        return randomNumbers;
     }
 }
