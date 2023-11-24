3aefcaa5d5f6e887a616fe7a3c12db618be96454
yooyoungpark <yoongtopiaa@gmail.com>
2023-11-24 14:45:55 +0900

    feat : 자동차 이름이 5글자를 초과한 경우 예외 발생

--- src/main/java/racingcar/domain/NumberGenerator.java
+++ src/main/java/racingcar/domain/NumberGenerator.java
@@ -1,9 +1,11 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class NumberGenerator {
     public int createRandomNumbers() {
-        return 0;
+        int randomNum = Randoms.pickNumberInRange(0,9);
+        return randomNum;
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,7 +9,14 @@ public class InputView {
     public static List<String> inputCarName(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String inputName = Console.readLine();
+        //쉼표(,)를 기준으로 구분
         List<String> inputCarNames = Arrays.asList(inputName.split(","));
+        //6자 이상으로 입력한 경우 예외 발생
+        for (String inputCarName : inputCarNames){
+            if (inputCarName.length() > 5){
+                throw new IllegalArgumentException("자동차 이름은 5자 이하로 입력하세요.");
+            }
+        }
         return inputCarNames;
     }
 
